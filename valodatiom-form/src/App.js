b7ceefbdbd3b4ef4a4ea4f4ba3ff119cbfda13cd
ParvejAalam <parvej.siddique@fugetroncorp.com>
2021-11-22 14:04:26 +0530

    updated

--- valodatiom-form/src/App.js
+++ valodatiom-form/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import Valid from "./Valid"
+import React, { useState } from "react";
 
-function App(props) {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [item, setItem] = useState([]);
+
+  const inputOnchange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const storeItem = () => {
+    if (inputValue !== "") {
+      item.push(inputValue);
+      setItem([...item]);
+      setInputValue("");
+    }
+  };
+  
+  const clearItem = () => {
+    item.pop(inputValue);
+    setItem([...item])
+  };
   return (
     <div>
-      <Valid/>
+      <input type="text" value={inputValue} onChange={inputOnchange} />
+      <button onClick={storeItem}>Show</button>
+      <button onClick={clearItem}>clear</button>
+      {item.map((element, index) => {
+        return <li key={index}>{element}</li>;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- valodatiom-form/src/Valid.js
+++ valodatiom-form/src/Valid.js
@@ -1,43 +1,14 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import InputMask from 'react-input-mask';
+import MaterialInput from '@material-ui/core/Input';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      margin:20,
-    },
-    form:{
-        width:"50%",
-       padding:20,
-       height:200,
-    }
-  }));
-  
-
-function Valid(props) {
-    const classes = useStyles();
-    
-    const isValid = () => {
-        console.log("clicked")
-        let name = document.getElementById("name").value;
-        if(name.length>4){
-            console.log("error")
-        }
-    }
-    return (
-        <div className = {classes.root}>
-            <form className={classes.form}>
-                <input type = "text" id = "name" placeholder = "enter your name" /><br/>
-                <input type = "email" id = "email" placeholder = "enter your email"/><br/>
-                <input type = "password" id = "password" placeholder = "enter your password"/><br/>
-                <input type = "tel" id = "tel" placeholder = "enter your mobileNumber"/><br/>
-                <input type = "tel" id = "confirmTel" placeholder = "confirm your mobileNumber"/><br/>
-                <Button onClick = {isValid} variant="contained" color="primary">
-                      submit
-                </Button>
-            </form>
-        </div>
-    );
-}
-
-export default Valid;
+ 
+// Will work fine
+const Input = (props) => {
+  return(
+  <InputMask mask="99/99/9999" value={props.value} onChange={props.onChange}>
+    {(inputProps) => <MaterialInput {...inputProps} type="tel" disableUnderline />}
+  </InputMask>
+);
+  }
+export default Input;
